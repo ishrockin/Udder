8115844ecdfe3c1a6492cd7d9c66de7c704fd490
Ishan Soni <ishrockin@ishans-macbook-pro.local>
2019-01-15 16:24:56 +0000

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ishan Soni on 15/01/2019.
+//  Copyright © 2019 Ishan Soni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
